7cca7d61ebba1acc67113ebea726d5b809601dd5
Justin Karneges <justin@affinix.com>
2012-06-13 12:26:17 -0700

    aspell

---
+++ aspell/build_package.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+set -e
+
+if [ $# != 4 ]; then
+	echo "usage: $0 [package] [arch] [prefix] [destdir]"
+	exit 1
+fi
+
+source ./package_info
+
+package_name=$1
+target_arch=$2
+base_prefix=$3
+destdir=$4
+
+target_platform=$target_arch-mingw32-gcc
+arch_prefix=$base_prefix/$target_arch
+pkgdir=$PWD/packages
+patchdir=$PWD/patches
+
+# make the install dir and ensure we can write to it
+mkdir -p $arch_prefix
+touch $arch_prefix/test_writable
+rm $arch_prefix/test_writable
+
+if [ "$target_arch" == "x86_64" ]; then
+	export PATH=/c/mingw64/bin:$PATH
+fi
+
+build_package() {
+	if [ ! -d "build/$2/$1" ]; then
+		echo "$1/$2: building..."
+		mkdir -p build/$2/$1
+		old_pwd=$PWD
+		cd build/$2/$1
+		build_package_$1
+		cd $old_pwd
+	else
+		echo "$1/$2: failed on previous run. remove the \"build/$2/$1\" directory to try again"
+		exit 1
+	fi
+}
+
+build_package_aspell() {
+	tar zxvf $pkgdir/$aspell_file
+	cd aspell-*
+	./configure --prefix=$arch_prefix
+	make
+	make install
+}
+
+build_package_aspell_en() {
+	tar zxvf $pkgdir/$aspell_en_file
+	cd aspell6-*
+	./configure --vars ASPELL=$arch_prefix/bin/aspell PREZIP=$arch_prefix/bin/prezip-bin
+	ASPELL_CONF="prefix /mingw/msys/1.0$arch_prefix" make
+	make install
+}
+
+build_package $package_name $target_arch
