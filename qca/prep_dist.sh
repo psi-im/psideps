8429d337d33fc96d15e1a787bd7dd072b40a2221
Justin Karneges <justin@affinix.com>
2012-07-25 19:23:18 -0700

    qca results need to be install_name_tool'd

--- qca/prep_dist.sh
+++ qca/prep_dist.sh
@@ -33,6 +33,27 @@ if [ "$platform" == "mac" ]; then
 	cp -a $target_base/plugins $target_dist_base
 
 	cp -a distfiles/mac/README $dist_base
+
+	QT_FRAMEWORKS="QtCore"
+
+	install_name_tool -id qca.framework/Versions/2/qca $target_dist_base/lib/qca.framework/qca
+
+	for g in $QT_FRAMEWORKS; do
+		install_name_tool -change $QTDIR/lib/$g.framework/Versions/4/$g $g.framework/Versions/4/$g $target_dist_base/lib/qca.framework/qca
+        done
+
+	install_name_tool -change $target_base/lib/qca.framework/Versions/2/qca qca.framework/Versions/2/qca $target_dist_base/bin/qcatool2
+	for g in $QT_FRAMEWORKS; do
+		install_name_tool -change $QTDIR/lib/$g.framework/Versions/4/$g $g.framework/Versions/4/$g $target_dist_base/bin/qcatool2
+	done
+
+	for p in `find $target_dist_base/plugins/crypto -name \*.dylib`; do
+		base_p=`basename $p`
+		install_name_tool -change $target_base/lib/qca.framework/Versions/2/qca qca.framework/Versions/2/qca $target_dist_base/plugins/crypto/$base_p
+		for g in $QT_FRAMEWORKS; do
+			install_name_tool -change $QTDIR/lib/$g.framework/Versions/4/$g $g.framework/Versions/4/$g $target_dist_base/plugins/crypto/$base_p
+		done
+	done
 else
 	TARGET_ARCHES="i386 x86_64"
 
