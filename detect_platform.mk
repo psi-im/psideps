c9ce8bb39b8c16962aec242ccabba285ae535cd1
Sergey <rion@Sergeys-iMac.local>
2017-06-02 13:32:49 +0500

    Fixed some errors

--- detect_platform.mk
+++ detect_platform.mk
@@ -5,12 +5,13 @@ ifeq ($(UNAME), Darwin)
 PLATFORM = mac
 else ifeq ($(findstring MINGW,$(UNAME)),MINGW)
 PLATFORM = win
-endif
+else
 PLATFORM =
+endif
 endif
 export PLATFORM
 
-ifeq($(PLATFORM),)
+ifeq ($(PLATFORM),)
 DEFAULT_TARGET=default-unknown
 else
 DEFAULT_TARGET=default-$(PLATFORM)
